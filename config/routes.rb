64ecb6201a7edd95b3b14d70c0958648f0791494
Jules Rivoire <julesriv@gmail.com>
2019-02-18 17:11:28 +0100

    ebauche de inbox

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   resources :tours do
     resources :bookings, only: [:new, :create]
   end
-  resources :bookings, only: :destroy
+  resources :bookings, only: [:destroy, :show]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
