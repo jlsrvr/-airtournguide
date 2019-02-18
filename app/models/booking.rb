3cb43b59ae4717055e8a3844ab042c5d766dade2
Jules Rivoire <julesriv@gmail.com>
2019-02-18 15:01:21 +0100

    model relationships done

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,6 @@
 class Booking < ApplicationRecord
   belongs_to :tour
   belongs_to :user
+  validates :status, inclusion: { in: %w[pending accepted declined] }
+  validates :date, presence: true
 end
--- app/models/tour.rb
+++ app/models/tour.rb
@@ -1,3 +1,8 @@
 class Tour < ApplicationRecord
   belongs_to :user
+  has_many :bookings, dependent: :destroy
+  validates :name, presence: true
+  validates :price, presence: true
+  validates :description, presence: true
+  validates :city, presence: true
 end
