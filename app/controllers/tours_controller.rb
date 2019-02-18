035948472f3027c4f8d1f04b62a57dcf9ea6f9ae
Yohan Bendavid <yohan.p002@gmail.com>
2019-02-18 16:36:12 +0100

    Actions tour controller

--- app/controllers/tours_controller.rb
+++ app/controllers/tours_controller.rb
@@ -1,2 +1,44 @@
 class ToursController < ApplicationController
+  def index
+    @tours = Tour.all
+  end
+
+  def show
+    @tour = Tour.find(params[:id])
+  end
+
+  def new
+    @tour = Tour.new
+  end
+
+  def edit
+    @tour = Tour.find(params[:id])
+  end
+
+  def create
+    @tour = Tour.new(tours_params)
+    if @tour.save
+      redirect_to tour_path(@tour)
+    else
+      render :new
+    end
+  end
+
+  def update
+    @tour = Tour.find(params[:id])
+    @tour.update(tours_params)
+    redirect_to tour_path(@tour)
+  end
+
+  def destroy
+    @tour = Tour.find(params[:id])
+    @tour.destroy
+    redirect_to tours_path
+  end
+
+  private
+
+  def tours_params
+    params.require(:tour).permit(:name, :price, :description, :city)
+  end
 end
