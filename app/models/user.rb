8073d046d7630f34b7492aaaf06a1f192a4ffbd0
Marine Sourin <marinesourin@live.fr>
2019-02-20 11:10:35 +0100

    Dashboard issues

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  has_many :bookings_as_guide, through: :tours, source: :bookings
+  # has_many :bookings_as_guide, through: :tours, source: :bookings
+  has_many :tours
   has_many :bookings
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
