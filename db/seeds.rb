ec83be5dce5a898f356d968dec6ff53468e56f1e
Yohan Bendavid <yohan.p002@gmail.com>
2019-02-20 16:07:57 +0100

    Padding-homepage

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+@tours = Tour.all
+@tours.each do |tour|
+tour.destroy
+end
