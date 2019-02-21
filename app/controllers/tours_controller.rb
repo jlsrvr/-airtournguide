3b1e87c626683ad7b95c633feab58d512c302782
Yohan Bendavid <yohan.p002@gmail.com>
2019-02-21 17:36:53 +0100

    Changements controllers, routes, création de la vue personnal_tours avec le scss et surement d'autres trucs que j'ai oublié

--- app/controllers/tours_controller.rb
+++ app/controllers/tours_controller.rb
@@ -16,6 +16,8 @@ class ToursController < ApplicationController
 
   def personal_index
     @tours = policy_scope(Tour)
+    @bookings = Booking.all.select { |booking| booking.tour.user == current_user }
+
     # @current_user_id = current_user.id.to_s
     # @tours = Tour.where(params[:user_id] = @current_user_id)
   end
