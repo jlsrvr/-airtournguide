9458df0bf4bf4183e42b54b4cb75c65271ca8d7f
Marine Sourin <marinesourin@live.fr>
2019-02-21 17:02:09 +0100

    Autocomplete on Search Bars added

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,8 +1,15 @@
 import "bootstrap";
 
+//________________________________________________
+
 import { loadDynamicBannerText } from '../components/banner';
 loadDynamicBannerText();
+
+//________________________________________________
+
 import "../plugins/flatpickr";
+
+//________________________________________________
 
 import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
 
@@ -10,6 +17,14 @@ import { initMapbox } from '../plugins/init_mapbox';
 
 initMapbox();
 
+//________________________________________________
+
 import { previewImageOnFileSelect } from "../components/photo_preview";
 
 previewImageOnFileSelect();
+
+//________________________________________________
+
+import { initAutocomplete } from "../plugins/init_autocomplete";
+
+initAutocomplete();
