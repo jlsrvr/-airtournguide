eb2e760c1662910fdbc89a68b6fc6695057a0571
Marine Sourin <marinesourin@live.fr>
2019-02-22 13:16:35 +0100

    Reviews done, guide/voyageur sides

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,5 +2,11 @@ class Review < ApplicationRecord
   belongs_to :user
   belongs_to :tour
 
-  validates :content, length: { minimum: 20 }
+  validates :content, length: { minimum: 20 }, presence: true
+  validates :user, presence: true
+  validates :rating, presence: true
+
+  def blank_stars
+   5 - rating.to_i
+  end
 end
